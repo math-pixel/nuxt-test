7ba5e977c61ed0a516db50ebf931efa8254bed34
math-pixel <59537610+math-pixel@users.noreply.github.com>
2024-03-08 11:45:20 +0100

    custom clicker page

--- pages/clickerCookie.vue
+++ pages/clickerCookie.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <Return></Return>
-    <h1 class="bg-red-500">Welcome to clicker cookie</h1>
-    <h2>your count : {{ clickCookie }}</h2>
+  <div class="text-[#FFEFD3] flex justify-between flex-col">
+    <Return class="absolute top-5 left-5"></Return>
+    <h1 class="">Welcome to clicker cookie</h1>
+    <h2>Your count : {{ clickCookie }}</h2>
     <clicker :URL="'/img/cookie.png'" @cliked="addCounter()"></clicker>
   </div>
 </template>
--- pages/clickerUnicorn.vue
+++ pages/clickerUnicorn.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <Return></Return>
+  <div class="text-[#FFEFD3] flex justify-between flex-col">
+    <Return class="absolute top-5 left-5" ></Return>
     <h1>Welcome to clicker Unicorn</h1>
     <h2>your counter : {{ clickUnicorn }}</h2>
     <clicker :URL="'/img/licorne.png'" @cliked="addCounter()"></clicker>
